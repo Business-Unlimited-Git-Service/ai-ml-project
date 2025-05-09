659ecd2e081e48e474e986a746f76fadd952a48c
CarlborHal <grwlp5k@gmail.com>
2025-05-09 10:18:04 -0400

    Co-authored-by: mendoul <mendoul@users.noreply.github.com> Co-authored-by: Jamie Tait <jamjota@users.noreply.github.com>

--- server/controllers/pineconeController.ts
+++ server/controllers/pineconeController.ts
@@ -1,48 +1,48 @@
 import { RequestHandler, Request, NextFunction } from 'express';
 import { CustomResponse } from '../types';
 import { Pinecone, ScoredPineconeRecord } from '@pinecone-database/pinecone';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 // console.log(process.env.PINECONE_API_KEY)
 const pinecone = new Pinecone({
-    apiKey: process.env.PINECONE_API_KEY,
+  apiKey: process.env.PINECONE_API_KEY as string,
 });
 
 export const queryPineconeDataBase: RequestHandler = async (
-    req: Request, 
-    res: CustomResponse, 
-    next: NextFunction
-) =>{
-    const { embedding } = res.locals;
+  req: Request,
+  res: CustomResponse,
+  next: NextFunction
+) => {
+  const { embedding } = res.locals;
 
-    if (!embedding) {
-        const error = {
-            log: 'Database query did not receive embedding',
-            code: 500,
-            message: { err: 'An error occured before querying database' },
-        };
-        return next(error);
-    }
-
-    try{
-        const index = pinecone.index('pokemon');
-            const queryResponse: { matches: ScoredPineconeRecord[] } = await index.query({
-                vector: embedding,
-                topK: 1,
-                includeMetadata: true
-            })
-            res.locals.pineconeQueryResult = queryResponse.matches.map(match => match.metadata);
-            next();
-            return
-        }
-
-    catch(err){    
-        return next({
-        log: `queryPineconeDatabase: ${new Error('Database query failed')}`,
-        status: 500,
-        message: { err: 'An error occurred while querying database' },
-      });
-    }
+  if (!embedding) {
+    const error = {
+      log: 'Database query did not receive embedding',
+      code: 500,
+      message: { err: 'An error occured before querying database' },
+    };
+    return next(error);
   }
+
+  try {
+    const index = pinecone.index('pokemon');
+    const queryResponse: { matches: ScoredPineconeRecord[] } =
+      await index.query({
+        vector: embedding,
+        topK: 1,
+        includeMetadata: true,
+      });
+      res.locals.pineconeQueryResult = queryResponse.matches.map((match) => match.metadata);
+
+    next();
+    return;
+  } catch (err) {
+    return next({
+      log: `queryPineconeDatabase: ${new Error('Database query failed')}`,
+      status: 500,
+      message: { err: 'An error occurred while querying database' },
+    });
+  }
+};
